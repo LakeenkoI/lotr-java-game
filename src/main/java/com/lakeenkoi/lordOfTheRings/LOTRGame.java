04119b067b960232c587f07323f44ff13b5c72df
Ilya Lakeenko <lakeen-999@mail.com>
2025-03-11 16:38:32 +0600

    update LOTRGame

--- src/main/java/com/lakeenkoi/lordOfTheRings/LOTRGame.java
+++ src/main/java/com/lakeenkoi/lordOfTheRings/LOTRGame.java
@@ -1,8 +1,16 @@
 package com.lakeenkoi.lordOfTheRings;
 
+import com.lakeenkoi.lordOfTheRings.controller.ControllerInput;
+
 public class LOTRGame {
 
     public static void main(String[] args) {
+        ControllerInput controllerInput = new ControllerInput();
 
+        controllerInput.selectRace()
+                .inputName()
+                .chooseWeapon()
+                .createEnemy()
+                .fight();
     }
 }
